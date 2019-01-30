575c851fe4ab0210857bd9b2c152e063c7a4b980
YueHsien <qwerbakecool80001@gmail.com>
2019-01-30 14:54:34 +0800

    記得密碼是大寫

--- login.php
+++ login.php
@@ -40,7 +40,8 @@ if ( isset($_POST["Password"]) )
 // 檢查是否輸入使用者名稱和密碼
 if ($username != "" && $password != "") {
    // 建立MySQL的資料庫連接 
-   $link = mysqli_connect("yuehsienmysql-mysqldbserver.mysql.database.azure.com", "sqladmin@yuehsienmysql-mysqldbserver","cd717630","music") 
+   $link = mysqli_connect("yuehsienmysql-mysqldbserver.mysql.database.azure.com","sqladmin@yuehsienmysql-mysqldbserver",
+                          "Cd717630","music")
         or die("無法開啟MySQL資料庫連接!<br/>");
    //送出UTF8編碼的MySQL指令
    mysqli_query($link, 'SET NAMES utf8'); 

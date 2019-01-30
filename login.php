b39e70f58961a65a9f96790d2b68de7c9040f0e8
YueHsien <qwerbakecool80001@gmail.com>
2019-01-30 14:18:49 +0800

    123

--- login.php
+++ login.php
@@ -41,7 +41,7 @@ if ( isset($_POST["Password"]) )
 if ($username != "" && $password != "") {
    // 建立MySQL的資料庫連接 
    $link = mysqli_connect("localhost","root",
-                          "cd717630","music")
+                          "","music")
         or die("無法開啟MySQL資料庫連接!<br/>");
    //送出UTF8編碼的MySQL指令
    mysqli_query($link, 'SET NAMES utf8'); 
